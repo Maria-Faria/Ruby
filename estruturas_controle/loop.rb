1cbc8f43261757ff8ace19d7125abfae8a927352
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-12-23 22:44:41 -0300

    doing a loop

---
+++ estruturas_controle/loop.rb
@@ -0,0 +1,13 @@
+# criar um contador
+# precisar de uma variável
+count = 0
+
+# loop de execução
+loop do  
+  puts count
+  # condição --> SE contador não for igual a 150
+  if count == 150
+    break
+  end
+  count += 1
+end

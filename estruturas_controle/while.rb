eb76d71d5d77156635bb0a2a24e37ea52e33c5dd
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-12-23 22:28:04 -0300

    counting numbers 1 to 100

---
+++ estruturas_controle/while.rb
@@ -0,0 +1,12 @@
+# comparar um número com uma condição
+num = 1
+
+# imprima todos os números a partir de 1 e menores que 100
+# execute esse código ENQUANTO num for menor que 100
+while num < 100
+  puts num
+
+  # precisa pegar esse num que ele imprimiu e seguir para o próximo
+  # utiliza o incremento (somar)
+  num += 1
+end

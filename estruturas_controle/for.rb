c30ea4cd11b4e8ef851b4470d4f9e33f27f051fb
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-12-23 22:23:37 -0300

    using for

---
+++ estruturas_controle/for.rb
@@ -0,0 +1,12 @@
+# leia uma lista (array) de elementos
+
+# criar a lista
+languages = ['Ruby', 'Go', 'C++', 'Python']
+count = 1
+
+# apareça na tela (puts) essa lista
+for language in languages
+  puts language
+  puts count
+  count += 1
+end
